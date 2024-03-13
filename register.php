038ee1ed73794deeda6e8c54f3287cb12db8f187
Ebtesham-21 <ebteshamhossain98@gmail.com>
2024-03-13 23:52:18 +0600

    register form done

--- register.php
+++ register.php
@@ -27,31 +27,39 @@
         <form id="loginForm" action="register.php" method="POST">
             <h2>Create your free account</h2>
             <p>
-                <label for="loginUsername">Username</label>
-                <input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. Simpson" required>
+                <label for="username">Username</label>
+                <input id="username" name="username" type="text" placeholder="e.g. Simpson" required>
 
             </p>
             <p>
-                <label for="loginUsername">Username</label>
-                <input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. Simpson" required>
+                <label for="firstName">First name</label>
+                <input id="firstName" name="firstName" type="text" placeholder="e.g. Bart" required>
 
             </p>
             <p>
-                <label for="loginUsername">Username</label>
-                <input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. Simpson" required>
+                <label for="lastName"> Last Name</label>
+                <input id="lastName" name="lastName" type="text" placeholder="e.g. Simpson" required>
 
             </p>
             <p>
-                <label for="loginUsername">Username</label>
-                <input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. Simpson" required>
+                <label for="email">Email</label>
+                <input id="email" name="email" type="email" placeholder="e.g. bart@gmail.com" required>
 
             </p>
             
             <p> 
+                 <label for="email2">Confirm Email</label>
+                 <input id="email2" name="email2" type="email"  required>
+            </p>
+            <p> 
                  <label for="loginPassword">Password</label>
                  <input id="loginPassword" name="loginPassword" type="password"  required>
             </p>
-            <button type="submit" name="registerButton">Register</button>
+            <p> 
+                 <label for="password2">Confirm Password</label>
+                 <input id="password2" name="password2" type="password"  required>
+            </p>
+            <button type="submit" name="registerButton">SIGN UP</button>
           
         </form>
 
