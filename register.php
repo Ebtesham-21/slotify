67fa8b7ad876c550e4fb18ff231e14cefdabd7e9
Ebtesham-21 <ebteshamhossain98@gmail.com>
2024-03-17 21:48:20 +0600

    class created

--- register.php
+++ register.php
@@ -1,105 +1,77 @@
 <?php 
-function sanitizeFormPassword($inputText){
-    $inputText = strip_tags($inputText);
-    return $inputText;
-
-}
-
-function sanitizeFromUsername($inputText) {
-    $inputText = strip_tags($inputText);
-    $inputText = str_replace(" ", "", $inputText);
-    return $inputText;
-}
-
-function sanitizeFormString($inputText) {
-    $inputText = strip_tags($inputText);
-    $inputText = str_replace(" ", "", $inputText);
-    $inputText = ucfirst(strtolower($inputText));
-    return $inputText;
-}
-if(isset($_POST['loginButton'])) {
-    
-}
-if(isset($_POST['registerButton'])) {
-    $username = sanitizeFormUsername($_POST['username']);
-    $firstName = sanitizeFormUsername($_POST['firstName']);
-    $lastName = sanitizeFormUsername($_POST['lastName']);
-    $email = sanitizeFormUsername($_POST['email']);
-    $email2 = sanitizeFormUsername($_POST['email2']);
-    $password = sanitizeFormUsername($_POST['password']);
-    $password2 = sanitizeFormUsername($_POST['password2']);
-
-
-
-}
+include("includes/classes/Account.php");
+$account = new Account();
+$account->register();
+include("includes/handlers/register-handler.php");
+include("includes/handlers/login-handler.php");
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome to Slotify</title>
+	<title>Welcome to Slotify!</title>
 </head>
 <body>
-    <div id="inputContainer">
-        <form id="loginForm" action="register.php" method="POST">
-            <h2>Login to your account</h2>
-            <p>
-                <label for="loginUsername">Username</label>
-                <input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. Simpson" required>
 
-            </p>
-            
-            <p> 
-                 <label for="loginPassword">Password</label>
-                 <input id="loginPassword" name="loginPassword" type="password" placeholder="Your Password" required>
-            </p>
-            <button type="submit" name="loginButton">Login</button>
-          
-        </form>
+	<div id="inputContainer">
+		<form id="loginForm" action="register.php" method="POST">
+			<h2>Login to your account</h2>
+			<p>
+				<label for="loginUsername">Username</label>
+				<input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. bartSimpson" required>
+			</p>
+			<p>
+				<label for="loginPassword">Password</label>
+				<input id="loginPassword" name="loginPassword" type="password" placeholder="Your password" required>
+			</p>
+
+			<button type="submit" name="loginButton">LOG IN</button>
+			
+		</form>
 
 
-        <form id="loginForm" action="register.php" method="POST">
-            <h2>Create your free account</h2>
-            <p>
-                <label for="username">Username</label>
-                <input id="username" name="username" type="text" placeholder="e.g. Simpson" required>
 
-            </p>
-            <p>
-                <label for="firstName">First name</label>
-                <input id="firstName" name="firstName" type="text" placeholder="e.g. Bart" required>
+		<form id="registerForm" action="register.php" method="POST">
+			<h2>Create your free account</h2>
+			<p>
+				<label for="username">Username</label>
+				<input id="username" name="username" type="text" placeholder="e.g. bartSimpson" required>
+			</p>
 
-            </p>
-            <p>
-                <label for="lastName"> Last Name</label>
-                <input id="lastName" name="lastName" type="text" placeholder="e.g. Simpson" required>
+			<p>
+				<label for="firstName">First name</label>
+				<input id="firstName" name="firstName" type="text" placeholder="e.g. Bart" required>
+			</p>
 
-            </p>
-            <p>
-                <label for="email">Email</label>
-                <input id="email" name="email" type="email" placeholder="e.g. bart@gmail.com" required>
+			<p>
+				<label for="lastName">Last name</label>
+				<input id="lastName" name="lastName" type="text" placeholder="e.g. Simpson" required>
+			</p>
 
-            </p>
-            
-            <p> 
-                 <label for="email2">Confirm Email</label>
-                 <input id="email2" name="email2" type="email"  required>
-            </p>
-            <p> 
-                 <label for="loginPassword">Password</label>
-                 <input id="loginPassword" name="loginPassword" placeholder="Your Password" type="password"  required>
-            </p>
-            <p> 
-                 <label for="password2">Confirm Password</label>
-                 <input id="password2" name="password2" type="password" placeholder="Your Password" required>
-            </p>
-            <button type="submit" name="registerButton">SIGN UP</button>
-          
-        </form>
+			<p>
+				<label for="email">Email</label>
+				<input id="email" name="email" type="email" placeholder="e.g. bart@gmail.com" required>
+			</p>
 
-    </div>
-    
+			<p>
+				<label for="email2">Confirm email</label>
+				<input id="email2" name="email2" type="email" placeholder="e.g. bart@gmail.com" required>
+			</p>
+
+			<p>
+				<label for="password">Password</label>
+				<input id="password" name="password" type="password" placeholder="Your password" required>
+			</p>
+
+			<p>
+				<label for="password2">Confirm password</label>
+				<input id="password2" name="password2" type="password" placeholder="Your password" required>
+			</p>
+
+			<button type="submit" name="registerButton">SIGN UP</button>
+			
+		</form>
+
+
+	</div>
+
 </body>
 </html>
