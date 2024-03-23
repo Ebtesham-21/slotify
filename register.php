de2ba3722e53207e2977fe6afe25922b3276b107
Ebtesham-21 <ebteshamhossain98@gmail.com>
2024-03-23 18:02:22 +0600

    icon

--- register.php
+++ register.php
@@ -17,8 +17,31 @@ function getInputValue($name){
 <head>
 	<title>Welcome to Slotify!</title>
 	<link rel="stylesheet" type="text/css" href="assets/css/register.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+	<script  src="assets/js/register.js"></script>
 </head>
 <body>
+	<?php 
+
+	if(isset($_POST['registerButton'])) {
+		echo '<script>
+				$(document).ready(function() {
+					$("#loginForm").hide();
+			    	$("#registerForm").show();
+				});
+			</script>';
+	}
+	else {
+		echo '<script>
+		$(document).ready(function() {
+			$("#loginForm").show();
+			$("#registerForm").hide();
+			});
+		</script>';
+
+	}
+
+	?>
 	<div id="background">
 		<div id="loginContainer">
 		<div id="inputContainer">
@@ -35,6 +58,9 @@ function getInputValue($name){
 			</p>
 
 			<button type="submit" name="loginButton">LOG IN</button>
+			<div class="hasAccountText">
+				<span id="hideLogin">Don't have an account? Signup here.</span>
+			</div>
 			
 		</form>
 
@@ -88,13 +114,24 @@ function getInputValue($name){
 			</p>
 
 			<button type="submit" name="registerButton">SIGN UP</button>
+			<div class="hasAccountText">
+				<span id="hideRegister">Already have an account? Login here.</span>
+			</div>
 			
 		</form>
 
 
 	</div>
+	<div id="loginText">
+		<h1>Get great music, right now</h1>
+		<h2>Listen to loads of songs for free</h2>
+		<ul>
+			<li>Discover music you'll fall in love with</li>
+		</ul>
 
-		</div>
+	</div>
+
+</div>
 	
 </div>
 
