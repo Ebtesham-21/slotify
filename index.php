d60be0a4258ca5e66f4fb1cad02eea4b1f6cca19
Ebtesham-21 <ebteshamhossain98@gmail.com>
2024-03-24 14:49:24 +0600

    icons added

--- index.php
+++ index.php
@@ -13,9 +13,36 @@ else {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
 </head>
 <body>
-    <h1>Hello</h1>
+    <div id="nowPlayingBarContainer">
+        <div id="nowPlayingBar">
+            <div id="nowPlayingLeft">
+
+            </div>
+            <div id="nowPlayingCenter">
+                <div class="content playerControls">
+                    <div class="buttons">
+                        <button class="controlButton shuffle">
+                            <img src="assets/images/icons/shuffle.png" alt="shuffle button">
+                        </button>
+
+                    </div>
+                     
+
+
+                </div>
+
+            </div>
+            <div id="nowPlayingRight">
+
+            </div>
+
+        </div>
+
+
+    </div>
     
 </body>
 </html>
