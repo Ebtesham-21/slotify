0e695e63d0d98aa8451727be84dc67ca813f8742
Ebtesham-21 <ebteshamhossain98@gmail.com>
2024-03-24 15:25:04 +0600

    button hover

--- index.php
+++ index.php
@@ -24,8 +24,25 @@ else {
             <div id="nowPlayingCenter">
                 <div class="content playerControls">
                     <div class="buttons">
-                        <button class="controlButton shuffle">
+                        <!-- title gula dei nai lagle dibo -->
+                        <button class="controlButton shuffle" >
                             <img src="assets/images/icons/shuffle.png" alt="shuffle button">
+                        </button>
+                      
+                        <button class="controlButton previous" >
+                            <img src="assets/images/icons/previous.png" alt="shuffle button">
+                        </button>
+                        <button class="controlButton play" >
+                            <img src="assets/images/icons/play.png" alt="shuffle button">
+                        </button>
+                        <button class="controlButton pause" style="display:none;" >
+                            <img src="assets/images/icons/pause.png" alt="shuffle button">
+                        </button>
+                        <button class="controlButton next" >
+                            <img src="assets/images/icons/next.png" alt="shuffle button">
+                        </button>
+                        <button class="controlButton repeat" >
+                            <img src="assets/images/icons/repeat.png" alt="shuffle button">
                         </button>
 
                     </div>
